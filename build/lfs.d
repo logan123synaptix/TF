7a65d87b29b1ef77d478a190f3865a7ddefba2da
ngoxuanloc2309 <ngoxuanloc23092004@gmail.com>
2026-07-20 08:30:32 +0700

    Remove build directory from repository

--- build/lfs.d
+++
@@ -1,8 +0,0 @@
-build/lfs.o: SynaptiX_FDK/services/littlefs/lfs.c \
- SynaptiX_FDK/services/littlefs/lfs.h \
- SynaptiX_FDK/services/littlefs/lfs_util.h \
- SynaptiX_FDK/services/logger/logger.h SynaptiX_FDK/app/app_config.h
-SynaptiX_FDK/services/littlefs/lfs.h:
-SynaptiX_FDK/services/littlefs/lfs_util.h:
-SynaptiX_FDK/services/logger/logger.h:
-SynaptiX_FDK/app/app_config.h:
